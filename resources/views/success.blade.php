79235089330d8de2ae576467e877c9e08e0629b9
林于斌 <papago75@gmail.com.tw>
2016-11-21 09:50:57 +0800

    修正載入js

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -10,25 +10,17 @@
 	</script>
 
 	<!--<link href="/css/app.css" rel="stylesheet">-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="{{ url('/css/bootstrap-theme.min.css') }}">
 	<link rel="stylesheet" href="{{url('/')}}/css/bootstrap.css">
 	<!--<link rel="stylesheet" href="{{url('/')}}/css/bootstrap-theme.css">-->
 	
-	<link rel="stylesheet" href="{{url('/')}}/css/bootstrap-datetimepicker.min.css">
 	<link rel="stylesheet" href="{{url('/')}}/css/jquery-ui.css">
-	<link rel="stylesheet" href="{{url('/')}}/css/sweetalert.css">
 	<!-- Fonts -->
 	<!--<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>-->
 	
 	<script src="{{url('/')}}/script/jquery-3.1.0.min.js"></script>
 	<script src="{{url('/')}}/script/jquery-ui.js?x=1"></script>
-	<script src="{{url('/')}}/script/sweetalert.js"></script>
 	<script src="{{url('/')}}/script/bootstrap.js"></script>
-	<script src="{{url('/')}}/script/jquery.blockUI.js"></script>
-	<script src="{{url('/')}}/script/jquery.form.min.js"></script>
-	<script src="{{url('/')}}/script/bootstrap-datetimepicker.min.js"></script>
-	<script src="{{url('/')}}/script/bootstrap-datetimepicker.zh-TW.js"></script>
-	<script src="{{url('/')}}/script/master.js"></script>
 </head>
 <body>
     <div class="container text-center">
